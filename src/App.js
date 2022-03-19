0824942227e8187c8cafa5f3ab596ee8e0de07d6
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2022-03-19 19:54:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import theme from "./components/UI/Theme";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
-import Try from "./components/Try";
 import Hero from "./components/Home";
 
 function App() {
